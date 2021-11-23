51860d8df463048678625c7ed7fb9bc7ec00c58a
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-11-23 15:03:19 +0200

    pridetas DELETE mygtukas

--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -10,9 +10,16 @@ function Item({ data, modal }) {
         <td>{data.is_busy}</td>
         <td>{data.last_use_time}</td>
         <td>{data.total_ride_kilometers}</td>
+        <td>
         <button className="btn btn-primary" onClick={showEdit}>
           Edit
         </button>
+        </td>
+        <td>
+        <button className="btn btn-danger">
+          Delete
+        </button>
+        </td>
       </tr>
     );
   }
