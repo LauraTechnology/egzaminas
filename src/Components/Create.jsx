4853763d5879acfa711c306d4cf1409d2f5ef9b4
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-11-24 13:36:51 +0200

    Create

--- src/Components/Create.jsx
+++ src/Components/Create.jsx
@@ -23,17 +23,21 @@ function Create() {
         <div className="scooter_form">
             <h2>Add another scooter</h2>
             <div className="scooter_form_input">
-                <span>Registration Code</span><input type="text" value={inputs.registrationCode} onChange={(e) => formControl(e, 'name')} />
+                <span>Registration Code</span><input type="text" value={inputs.registration_code} onChange={(e) => formControl(e, 'registration_code')} />
             </div>
-            <div>
-                <span></span><input type="text" />
+            <div className="scooter_form_input">
+                <span>Is Busy</span><input type="text" value={inputs.is_busy} onChange={(e) => formControl(e, 'is_busy')} />
             </div>
-            <div>
-                <span></span><input type="text" />
+            <div className="scooter_form_input">
+                <span>Last Time Used</span><input type="date" value={inputs.last_use_time} onChange={(e) => formControl(e, 'last_use_time')} />
             </div>
-            <div>
-                <span></span><input type="text" />
+            <div className="scooter_form_input">
+                <span>Total Ride Kilometers</span><input type="text" value={inputs.total_ride_kilometers} onChange={(e) => formControl(e, 'total_ride_kilometers')} />
             </div>
+            <div className="scooter_form_input">
+                <button onClick={handleCreate}>Add SCOOTER</button>
+            </div>
+
 
         </div>
       
