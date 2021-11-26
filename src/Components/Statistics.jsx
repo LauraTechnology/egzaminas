3cd7f49738f08777e03874e529e3103c2f23dfaa
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-11-26 20:18:40 +0200

    <span> DONE

--- src/Components/Statistics.jsx
+++ src/Components/Statistics.jsx
@@ -3,11 +3,11 @@
 function ScooterStatistics() {
 
     return (
-        <div>
-            <div>
-                <span></span>
-                <span></span>
-                <span></span>
+        <div className="scooter__statistics">
+            <div className="zoo__statistics__stat">
+                <span><i>Scooters Count:</i><b>{statistics.count}</b></span>
+                <span><i>Scooters Ridden Km:</i><b>{statistics.riddenKm.tofixed(2)} Km</b></span>
+                <span><i>Scooters Average Ridden Km:</i><b>{statistics.averageRiddenKm.toPrecision(5)} Km</b></span>
             </div>
             <div>
 
