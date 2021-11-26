727df215c9b15b3c92744a81d321e53f275c975e
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-11-26 13:04:07 +0200

    is_busy išimtas

--- src/Components/Create.jsx
+++ src/Components/Create.jsx
@@ -4,7 +4,6 @@ function Create({create}) {
 
     const [inputs, setInputs] = useState ({
         registration_code: "",
-        is_busy: "",
         last_use_time: "",
         total_ride_kilometers: "",
     })
@@ -19,7 +18,6 @@ function Create({create}) {
         create(inputs);
         setInputs({
             registration_code: "",
-            is_busy: "",
             last_use_time: "",
             total_ride_kilometers: ""
 
@@ -32,9 +30,6 @@ function Create({create}) {
             <h2>Add another scooter</h2>
             <div className="scooter_form_input">
                 <span>Registration Code</span><input type="text" value={inputs.registration_code} onChange={(e) => formControl(e, 'registration_code')} />
-            </div>
-            <div className="scooter_form_input">
-                <span>Is Busy</span><input type="text" value={inputs.is_busy} onChange={(e) => formControl(e, 'is_busy')} />
             </div>
             <div className="scooter_form_input">
                 <span>Last Time Used</span><input type="date" value={inputs.last_use_time} onChange={(e) => formControl(e, 'last_use_time')} />
