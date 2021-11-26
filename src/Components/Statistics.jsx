fcfb22f1b92eb6b101f3b600413c30809d7b1f4a
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-11-26 21:35:58 +0200

    -

--- src/Components/Statistics.jsx
+++ src/Components/Statistics.jsx
@@ -9,7 +9,7 @@ function ScooterStatistics() {
                 <span><i>Scooters Ridden Km:</i><b>{statistics.riddenKm.tofixed(2)} Km</b></span>
                 <span><i>Scooters Average Ridden Km:</i><b>{statistics.averageRiddenKm.toPrecision(5)} Km</b></span>
             </div>
-            <div>
+            <div className="zoo__statistics_gstat">
 
             </div>
         </div>
