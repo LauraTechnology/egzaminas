a07508cb1458eaf9cb4c778e8d7f923eb171d646
Laura Zukaite <laura.zukaite0912@gmail.com>
2021-11-26 20:08:21 +0200

    New Component: ScooterStatistics

---
+++ src/Components/Statistics.jsx
@@ -0,0 +1,19 @@
+// Arvydo pavizdyje Stats == Statistics https://github.com/A-Kija/liutas/blob/main/src/Components/ZooStats.jsx
+
+function ScooterStatistics() {
+
+    return (
+        <div>
+            <div>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default ScooterStatistics;
